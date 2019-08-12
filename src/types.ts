dcb621666d01f79d7ccbf751543ceddfd91f93b9
Jesse Luoto <jesse.luoto@gmail.com>
2019-08-12 23:11:10 +0300

    WIP: Add loan type

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,8 @@ export const Loan = t.type({
   name: t.string,
   installment: t.number,
   leftover: t.number,
-  interest: t.number
+  interest: t.number,
+  type: t.intersection([t.literal('annuity'), t.literal('annuity')])
 })
 
 export type LoanType = t.TypeOf<typeof Loan>
